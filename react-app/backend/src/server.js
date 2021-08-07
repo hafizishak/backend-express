61f3adf2067706016d324bd60ae5c91a49d5a5dc
hafizishak <mhi03037@gmail.com>
2021-08-07 22:19:59 +0800

    Add commit

--- react-app/backend/src/server.js
+++ react-app/backend/src/server.js
@@ -1,22 +1,21 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
-const articlesInfo = {
-    'learn-react': {
-        upvotes: 0,
-        comments: []
-    },
-    'learn-node': {
-        upvotes: 0,
-        comments: []
-    },
-    'learn-angular': {
-        upvotes: 0,
-        comments: []
-    }
-}
+import { MongoClient } from 'mongodb';
 
 const app = express();
+
+const withDB = async (operations, res) => {
+    try{
+        const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true});
+        const db = client.db('my-blog');
+
+        await operations(db);
+
+        client.close();        
+    }catch(error){
+        res.status(500).json({ message: 'Error connecting to db', error });
+    }
+}
 
 app.use(bodyParser.json());
 
@@ -25,20 +24,48 @@ app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}!`));
 
 app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}`));
 
-app.post('/api/articles/:name/upvotes', (req, res) => {
-    const articleName = req.params.name;
+app.get('/api/articles/:name', async (req, res) => {
+    withDB(async (db) => {
+        const articleName = req.params.name;
 
-    articlesInfo[articleName].upvotes += 1;
-    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`);
+        const articleInfo = await db.collection('articles').findOne({name: articleName});
+        res.status(200).json(articleInfo);        
+    }, res) 
 })
 
-app.post('/api/articles/:name/addComment', (req, res) => {
-    const {username, comment} = req.body;
-    const articleName = req.params.name;
-    console.log('art: ', articleName);
-    articlesInfo[articleName].comments.push({username, comment});
+app.post('/api/articles/:name/upvotes', async (req, res) => {
+    withDB(async (db) => {
+        const articleName = req.params.name;
+        const articleInfo = await db.collection('articles').findOne({ name: articleName});
 
-    res.status(200).send(articlesInfo[articleName]);
+        await db.collection('articles').updateOne({name: articleName}, {
+            '$set': {
+                upvotes: articleInfo.upvotes + 1,
+            },
+        });
+
+        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName});
+        
+        res.status(200).json(updatedArticleInfo);
+    }, res)
+})
+
+app.post('/api/articles/:name/addComment', async (req, res) => {
+    withDB(async (db) => {
+        const {username, comment} = req.body;
+        const articleName = req.params.name;
+        const articleInfo = await db.collection('articles').findOne({ name: articleName});
+
+        await db.collection('articles').updateOne({name: articleName}, {
+            '$set': {
+                comments: articleInfo.comments.concat({username, comment})
+            },
+        });
+
+        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName});
+        
+        res.status(200).json(updatedArticleInfo);
+    }, res)
 })
 
 app.listen(8000, () => console.log('Listening on port 8000'));
---
+++ react-app/backend/src/serverBackup.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
+
+const articlesInfo = {
+    'learn-react': {
+        upvotes: 0,
+        comments: []
+    },
+    'learn-node': {
+        upvotes: 0,
+        comments: []
+    },
+    'learn-angular': {
+        upvotes: 0,
+        comments: []
+    }
+}
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/hello', (req, res) => res.send('Hello!'));
+app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}!`));
+
+app.post('/hello', (req, res) => res.send(`Hello ${req.body.name}`));
+
+app.get('/api/articles/:name', (req, res) => {
+    const articleName = req.params.name;
+
+    articlesInfo[articleName].upvotes += 1;
+    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`);
+})
+
+app.post('/api/articles/:name/upvotes', (req, res) => {
+    const articleName = req.params.name;
+
+    articlesInfo[articleName].upvotes += 1;
+    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`);
+})
+
+app.post('/api/articles/:name/addComment', (req, res) => {
+    const {username, comment} = req.body;
+    const articleName = req.params.name;
+    console.log('art: ', articleName);
+    articlesInfo[articleName].comments.push({username, comment});
+
+    res.status(200).send(articlesInfo[articleName]);
+})
+
+app.listen(8000, () => console.log('Listening on port 8000'));
